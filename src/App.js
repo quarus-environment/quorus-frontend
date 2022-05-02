c71e4929a03c487b94ac68d5131e7ea7d6561563
lenaa1 <85735585+lenaa1@users.noreply.github.com>
2022-05-02 18:57:18 +0500

    repaired code style

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import './App.css';
-import {useState} from "react";
-import {Header, HowItWorks} from "./components";
-import {Main} from "./components";
-
-
-
-/*export function App() {
-    const [name, setName] = usete("приветик :)")
-  return (
-      <div className='App'>
-          <div className='Header' onClick={() => {
-              setName("у тебя будет ребенок!!!!!!!!!!!")
-          }}>
-              {name}
-          </div>
-      </div>
-  );
-}*/
+import { useState } from "react";
+import { Header, HowItWorks } from "./components";
+import { Main } from "./components";
 
 export function App() {
-
     return(
-
-                 <div>
-                     <Header />
-                     <Main />
-                     <HowItWorks/>
-                 </div>
-
-
-
-
+        <div>
+            <Header />
+            <Main />
+            <HowItWorks/>
+        </div>
     )
 }
